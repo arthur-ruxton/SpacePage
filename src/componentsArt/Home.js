a3f2746123bb4a3608cbf2a443d71114c78df0d6
eddie ruxton <ae.ruxton@gmail.com>
2021-10-28 10:31:25 +0100

    image of the day added to homescreen, scss file added for home page

--- src/componentsArt/Home.js
+++ src/componentsArt/Home.js
@@ -1,8 +1,11 @@
+import BackgroundGetter from "../componentsAstro/BackgroundGetter"
+
 const Home = () => {
     return (
-      <section>
+      <section className="home-section">
         <h4> Welcome to Space News</h4>
         <p> Here we venture into the beyond, explore whats happening in the space around us and look at some pretty far away stuff</p>
+        <BackgroundGetter />
       </section>
     )
   }
